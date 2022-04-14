24b9b557f5b34ea12f20825eb5ee9d0486f6d5a3
khj5014 <64896739+khj5014@users.noreply.github.com>
2022-04-14 16:36:20 +0900

    수바

--- Project5_1/app/src/main/java/com/example/project5_1/FragActivity.java
+++ Project5_1/app/src/main/java/com/example/project5_1/FragActivity.java
@@ -1,17 +1,45 @@
 package com.example.project5_1;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FragActivity extends AppCompatActivity {
+
+    OneFragment fragment1;
+    TwoFragment fragment2;
+    ThreeFragment fragment3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frag);
 
+        fragment1 = new OneFragment();
+        fragment2 = new TwoFragment();
+        fragment3 = new ThreeFragment();
 
+        findViewById(R.id.btn1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame1, fragment1).commit();
+            }
+        });
+
+        findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame1, fragment2).commit();
+            }
+        });
+
+        findViewById(R.id.btn3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame1, fragment3).commit();
+            }
+        });
 
 
     }
