441f9cf8de06d934369b5e796559520394ce71d9
khj5014 <64896739+khj5014@users.noreply.github.com>
2022-04-14 15:57:54 +0900

    메롱

--- Project5_1/app/src/main/java/com/example/project5_1/InflateActivity.java
+++ Project5_1/app/src/main/java/com/example/project5_1/InflateActivity.java
@@ -1,21 +1,67 @@
 package com.example.project5_1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class InflateActivity extends AppCompatActivity {
 
     TextView textView1, textView2, textView3;
-    FrameLayout
+    FrameLayout frame;
+    LayoutInflater inflater;
+    View view;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inflate);
 
-    addCh
+        addChangeView(0);
+        findViewById(R.id.btn1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addChangeView(0);
+            }
+        });
+        findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addChangeView(1);
+            }
+        });
+        findViewById(R.id.btn3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addChangeView(2);
+            }
+        });
+    }
 
+    private void addChangeView(int index) {
+        frame = findViewById(R.id.frame1);
+        inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (frame.getChildCount() > 0) frame.removeViewAt(0);
+
+
+        switch (index) {
+            case 0:
+                view = inflater.inflate(R.layout.page1, frame, false);
+                break;
+            case 1:
+                view = inflater.inflate(R.layout.page2, frame, false);
+                break;
+            case 2:
+                view = inflater.inflate(R.layout.page3, frame, false);
+                break;
+        }
+        if (view != null) {
+            frame.addView(view);
+        }
     }
 }
