e8b431cee74572293d42ac685aa873817acba9ca
khj5014 <64896739+khj5014@users.noreply.github.com>
2022-04-21 16:45:19 +0900

    pro7_1 add

--- Project6_1/app/src/main/java/com/example/project6_1/MainActivity.java
+++ Project6_1/app/src/main/java/com/example/project6_1/MainActivity.java
@@ -1,14 +1,108 @@
 package com.example.project6_1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Chronometer chronometer1;
+    Button btnStart, btnEnd;
+    RadioButton rdoCal, rdoTime;
+    CalendarView calView;
+    TimePicker tPicker;
+    TextView textView1;
+
+    int selectYear, selectMonth, selectDay , selectHour, selectMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnStart = findViewById(R.id.btnStart);
+        btnEnd = findViewById(R.id.btnEnd);
+        rdoCal = findViewById(R.id.rdoCal);
+        rdoTime = findViewById(R.id.rdoTiem);
+        chronometer1 = findViewById(R.id.chronometer1);
+        tPicker = findViewById(R.id.timePicker);
+
+        textView1 = findViewById(R.id.textView1);
+        calView = findViewById(R.id.calendarView1);
+
+        tPicker.setVisibility(View.INVISIBLE);
+        calView.setVisibility(View.INVISIBLE);
+
+        rdoCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tPicker.setVisibility(View.INVISIBLE);
+                calView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        rdoTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tPicker.setVisibility(View.VISIBLE);
+                calView.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chronometer1.setBase(SystemClock.elapsedRealtime());
+                chronometer1.start();
+                chronometer1.setTextColor(Color.RED);
+            }
+        });
+
+        btnEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chronometer1.stop();
+                chronometer1.setTextColor(Color.BLUE);
+
+                textView1.setText(Integer.toString(selectYear) + "년"
+                        + Integer.toString(selectMonth) + "월"
+                        + Integer.toString(selectDay) + "일"
+                        + Integer.toString(selectHour) + "시"
+                        + Integer.toString(selectMinute) + "분에 예약완료"
+
+                );
+            }
+        });
+
+        tPicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int Hour, int Minute) {
+                selectHour = Hour;
+                selectMinute = Minute;
+            }
+        });
+
+        calView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
+
+                selectYear = year;
+                selectMonth = month + 1;
+                selectDay = dayOfMonth;
+
+            }
+        });
+
     }
 }
