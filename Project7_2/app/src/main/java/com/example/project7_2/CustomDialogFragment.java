213623d1132d459ed3ba37aeb7aa32b1d260a740
khj5014 <64896739+khj5014@users.noreply.github.com>
2022-05-12 17:10:44 +0900

    end

--- Project7_2/app/src/main/java/com/example/project7_2/CustomDialogFragment.java
+++ Project7_2/app/src/main/java/com/example/project7_2/CustomDialogFragment.java
@@ -1,5 +1,7 @@
 package com.example.project7_2;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -55,10 +61,58 @@ public class CustomDialogFragment extends Fragment {
         }
     }
 
+    TextView tvName, tvEmail, toastText;
+    Button btnDialog;
+    EditText dlgName, dlgEmail;
+    View dialogView, toastView;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_custom_dialog, container, false);
+        ViewGroup rootview = (ViewGroup) inflater.inflate(R.layout.fragment_custom_dialog, container, false);
+
+        tvName = rootview.findViewById(R.id.tvName);
+        tvEmail = rootview.findViewById(R.id.tvEmail);
+
+        btnDialog = rootview.findViewById(R.id.btn_custom_Dialog);
+
+        btnDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                dialogView = View.inflate(getActivity(), R.layout.dialog1, null);
+                dlgName = (EditText) dialogView.findViewById(R.id.dlgName);
+                dlgEmail = (EditText) dialogView.findViewById(R.id.dlgEmail);
+
+                AlertDialog.Builder dialog = new AlertDialog.Builder(getActivity());
+                dialog.setTitle("사용자 정보입력");
+                dialog.setIcon(R.drawable.icon_custom_dialog);
+                dialog.setView(dialogView);
+                dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        tvName.setText(dlgName.getText().toString());
+                        tvEmail.setText(dlgEmail.getText().toString());
+                    }
+                });
+                dialog.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast toast = new Toast(getActivity());
+                        toastView = View.inflate(getActivity(),R.layout.toast1,null);
+                        toastText = (TextView) toastView.findViewById(R.id.toastText);
+                        toastText.setText("취소했습니다.");
+                        toast.setView(toastView);
+                        toast.show();
+                    }
+                });
+                dialog.show();
+            }
+        });
+
+        return rootview;
     }
 }
