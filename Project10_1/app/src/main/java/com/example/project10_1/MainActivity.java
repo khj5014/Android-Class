e74d6afba06e66cff20bb555add873fc71a2f5f2
khj5014 <64896739+khj5014@users.noreply.github.com>
2022-05-19 17:05:30 +0900

    명화 랭킹 순위 앱

--- Project10_1/app/src/main/java/com/example/project10_1/MainActivity.java
+++ Project10_1/app/src/main/java/com/example/project10_1/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,12 +17,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("명화 선호도 조사");
+        int voteCount[] = new int[9];
+
+        for (int i = 0; i < 9; i++)
+            voteCount[i] = 0;
+        Integer ImageId[] = {R.id.iv1, R.id.iv2, R.id.iv3, R.id.iv4, R.id.iv5, R.id.iv6, R.id.iv7, R.id.iv8, R.id.iv9};
+
+        final String imgName[] = {"독서하는 소녀", "꽃장식 모자 소녀", "부채를 든 소녀",
+
+                "이레느깡 단 베르양", "잠자는 소녀", "테라스의 두 자매", "피아노 레슨", "피아노 앞의 소녀들",
+
+                "해변에서"};
+
+        ImageView image[] = new ImageView[9];
+        for (int i = 0; i < ImageId.length; i++) {
+            int index = i;
+            image[index] = findViewById(ImageId[i]);
+
+            image[index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    voteCount[index]++;
+                    Toast.makeText(MainActivity.this, imgName[index] + ": " + voteCount[index] + "표를 선택", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
 
         btnResult = findViewById(R.id.btnResult);
         btnResult.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+
+                intent.putExtra("VoteCount", voteCount);
+                intent.putExtra("ImgName", imgName);
+
                 startActivity(intent);
             }
         });
--- Project10_1/app/src/main/java/com/example/project10_1/ResultActivity.java
+++ Project10_1/app/src/main/java/com/example/project10_1/ResultActivity.java
@@ -6,6 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ResultActivity extends AppCompatActivity {
     Button btnReturn;
@@ -14,6 +19,43 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        setTitle("투표 결과");
+
+        Intent intent = getIntent();
+        int[] voteResult = intent.getIntArrayExtra("VoteCount");
+        String[] imageName = intent.getStringArrayExtra("ImgName");
+
+        TextView tvTop = findViewById(R.id.tvTop);
+        ImageView ivTop = findViewById(R.id.ivTop);
+
+        Integer imageID[] = {R.drawable.pic1, R.drawable.pic2, R.drawable.pic3, R.drawable.pic4, R.drawable.pic5, R.drawable.pic6, R.drawable.pic7, R.drawable.pic8, R.drawable.pic9};
+
+        int max = 0;
+        for (int i = 0; i < voteResult.length; i++) {
+            if (voteResult[max] < voteResult[i]) {
+                max = i;
+            }
+        }
+
+        tvTop.setText(imageName[max]);
+        ivTop.setImageResource(imageID[max]);
+
+
+        TextView tv[] = new TextView[imageName.length];
+        RatingBar rbar[] = new RatingBar[imageName.length];
+
+        Integer tvID[] = {R.id.tv1, R.id.tv2, R.id.tv3, R.id.tv4, R.id.tv5, R.id.tv6, R.id.tv7, R.id.tv8, R.id.tv9};
+        Integer rbarID[] = {R.id.rbar1, R.id.rbar2, R.id.rbar3, R.id.rbar4, R.id.rbar5, R.id.rbar6, R.id.rbar7, R.id.rbar8, R.id.rbar9};
+
+        for (int i = 0; i < voteResult.length; i++) {
+            tv[i] = (TextView) findViewById(tvID[i]);
+            rbar[i] = (RatingBar) findViewById(rbarID[i]);
+        }
+
+        for (int i = 0; i < voteResult.length; i++) {
+            tv[i].setText(imageName[i]);
+            rbar[i].setRating((float) voteResult[i]);
+        }
 
         btnReturn = findViewById(R.id.btnReturn);
         btnReturn.setOnClickListener(new View.OnClickListener() {
